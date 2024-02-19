0d7d8f96bf14427bf9b5acb2d5b78b41d47e7074
Rodrigo Pires <pirex02@gmail.com>
2024-02-19 19:46:38 +0000

    solved all exercices

--- 06_leapYears/leapYears.js
+++ 06_leapYears/leapYears.js
@@ -1,5 +1,7 @@
-const leapYears = function() {
-
+const leapYears = function (leapYears) {
+  return (
+    leapYears % 4 === 0 && (leapYears % 100 !== 0 || leapYears % 400 === 0)
+  );
 };
 
 // Do not edit below this line
