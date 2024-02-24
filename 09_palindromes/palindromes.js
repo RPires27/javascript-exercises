e618d531a2840f4c89133a6f3c714f101d4b93bf
Rodrigo Pires <pirex02@gmail.com>
2024-02-24 11:45:05 +0000

    solved exercices

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,9 @@
-const palindromes = function () {
+const palindromes = function (string) {
+  let regex = /[\W_]/g;
+  let lowerCaseString = string.toLowerCase().replace(regex, "");
+  let reversedString = lowerCaseString.split("").reverse().join("");
 
+  return lowerCaseString === reversedString;
 };
 
 // Do not edit below this line
